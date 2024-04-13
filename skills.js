8ec6976b2d274e22f5fba3db57ef07299e0e6732
Marco Tignanelli <93273771+Mark-kus@users.noreply.github.com>
2024-04-13 15:34:31 +0000

    si

--- skills.js
+++ skills.js
@@ -1,3 +1,7 @@
 function calculateNumbers(var1, var2) {
-  return var1 + var2;
+    return var1 + var2;
 }
+
+console.log(calculateNumbers(5, 10)); // Output: 15
+console.log(calculateNumbers(-3, 7)); // Output: 4
+console.log(calculateNumbers(0, 0)); // Output: 0
